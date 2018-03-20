aad82c08233c3a08adbedadb35e5559c60e0e2f4
Kayla Cross <kac288@georgetown.edu>
2018-03-20 03:19:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,18 @@
 class School
 
+  attr_reader :roster
+
   ROSTER = {}
 
   def initialize(name)
     @name = name
   end
+
+  def add_student(name, grade)
+    if ROSTER.key?(grade)
+      ROSTER << name
+  end
+
 end
 
 school = School.new("Greendale Community College")
